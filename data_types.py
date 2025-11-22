eaeab7b1b8e1188af5a3d931e03f8be96e22dbb8
Kalyan5708 <yesala.kalyan@gmail.com>
2025-11-22 09:48:43 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,16 @@
+#data types - in python 3 data typoes
+#1.int(integer)
+#str(string)
+#3float(decimals)
+name="kalyan122"
+age=30
+salary=90.54
+data="1234"
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(salary)
+print(type(salary))
+print(data)
+print(type(data))
---
+++ variables.py
@@ -0,0 +1,7 @@
+#variables-is a memory allocation to a value 
+name="kalyan"
+age=16
+salary=150000.00
+print(name)
+print(age)
+print(salary)
